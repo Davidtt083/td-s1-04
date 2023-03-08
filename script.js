91c3a3c18d87858a403a2c501cd18a9a735c7413
Davidtt083 <davidtt083@gmail.com>
2023-03-08 11:09:11 -0600

    ya quedó

--- script.js
+++ script.js
@@ -43,25 +43,32 @@ jQuery(window).resize(function () {
 function mostrar(){
 	var element = document.getElementById("text1");
 	var element2 = document.getElementById("btn2");
+	var element3 = document.getElementById("text3");
+	var element4 = document.getElementById("text2");
 	element.classList.remove("hide");
 	element2.classList.remove("novisited");
 	element2.setAttribute('onclick','mostrar2()');
+	element3.classList.add("hide");
+	element4.classList.add("hide");
 }
 
 function mostrar2(){
 	var element = document.getElementById("text1");
 	var element2 = document.getElementById("text2");
 	var element3 = document.getElementById("btn3");
+	var element4 = document.getElementById("text3");
 	element.classList.add("hide");
 	element2.classList.remove("hide");
 	element3.classList.remove("novisited");
 	element3.setAttribute('onclick','mostrar3()');
+	element4.classList.add("hide");
 }
 
 function mostrar3(){
 	var element = document.getElementById("text3");
 	var element2 = document.getElementById("text2");
-	var element3 = document.getElementById("btn3");
+	var element3 = document.getElementById("text1");
 	element.classList.remove("hide");
 	element2.classList.add("hide");
+	element3.classList.add("hide");
 }
